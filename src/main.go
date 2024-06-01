929c7bc52c19468c961d98487f4a82ee49ee499f
adamjsturge <adamjsturge@gmail.com>
2024-06-01 16:15:46 -0700

    Add Things I normally add

--- src/main.go
+++ src/main.go
@@ -3,7 +3,7 @@ package main
 import "github.com/bwmarrin/discordgo"
 
 func main() {
-	discord, err := discordgo.New("Bot " + "authentication token")
+	_, err := discordgo.New("Bot " + "authentication token")
 	if err != nil {
 		panic(err)
 	}
